0d01a1705bd42939f7810a14705b312929abc0f0
Rodrigo Gomes <rgomes.rodrigo@gmail.com>
2012-07-20 14:44:20 -0300

    created a page to test jquery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajquery::Application.config.secret_token = '97b2eb7300187bfa7c1d4bcd6e79941fc71d715f13844d16a411ffdcbf2ef0a66432ae72be3fbfd7fefd3cdf9232119b93011c9f9cda8d26f850ab8ad911b5e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ajquery::Application.config.session_store :cookie_store, key: '_ajquery_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ajquery::Application.config.session_store :active_record_store
